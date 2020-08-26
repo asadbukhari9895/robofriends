9ba607312d7192b174e332ed8ec6226911b8723b
Bukhari <asadbukhari9895@gmail.com>
2020-08-26 16:14:24 +0500

    adding files

---
+++ build/precache-manifest.3e3271c6a468ed6fd2aeda89a457acc5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4829a279ee58123904aa61f7ac5cebf9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "5ed63e8af1205de1b48b",
+    "url": "/static/css/main.d57b06a5.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ed63e8af1205de1b48b",
+    "url": "/static/js/main.ca439fea.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
